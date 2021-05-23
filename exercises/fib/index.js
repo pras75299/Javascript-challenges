b9e0aa280184d21651f4ea0fec01fed65da3ef57
pras75299 <singh.prashantking@gmail.com>
2021-05-23 20:15:00 +0530

    Solution-2 Fib Problem

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,9 +8,12 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
+function fib(n, memo = {}) {
+    if(n in memo) return memo[n];
     if(n <= 2) return 1;
-    return fib(n - 1) + fib(n - 2);
+
+    memo[n] = fib(n - 1, memo) + fib(n - 2, memo)
+    return memo[n];     
 }
 
 module.exports = fib;
